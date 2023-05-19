cbdff08ae0895006de7b13dab270e47042aada1e
Jose <jose.mar.123@hotmail.com>
2023-05-18 22:08:22 -0600

    Modify Contact component so it works with props
    Added 3 more contact cards and modified contact component to gather
    its data from props instead of being hard coded.

--- AirBnB Experiences Clone/src/App.jsx
+++ AirBnB Experiences Clone/src/App.jsx
@@ -3,13 +3,45 @@ import Contact from "./components/Contact"
 import HeroSection from "./components/HeroSection"
 import NavBar from "./components/NavBar"
 
+import mrWhiskeronImg from './assets/cats/mr-whiskerson.png'
+import fluffykins from './assets/cats/fluffykins.png'
+import felix from './assets/cats/felix.png'
+import pumpkin from './assets/cats/pumpkin.png'
+
+
+
+
+
 function App() {
   return (
     <>
       <NavBar />
-      <Contact />
-      {/* <HeroSection /> */}
-      {/* <Card /> */}
+      <div className="contact-container">
+        <Contact 
+        img={mrWhiskeronImg} 
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+        />
+        <Contact 
+        img={fluffykins}
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+        />
+        <Contact 
+        img={felix} 
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+        />
+        <Contact 
+        img={pumpkin}
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+        />
+      </div>
     </>
   )
 }
--- AirBnB Experiences Clone/src/components/Contact.jsx
+++ AirBnB Experiences Clone/src/components/Contact.jsx
@@ -2,19 +2,19 @@ import catImage from '../assets/cats/mr-whiskerson.png'
 import phoneIcon from '../assets/cats/phone-icon.png'
 import mailIcon from '../assets/cats/mail-icon.png'
 
-export default function Contact(){
+export default function Contact(props){
     return(
         <div className='contacts'>
             <div className="contact-card">
-            <img src={catImage}/>
-            <h3>Mr. Whiskerson</h3>
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
             <div className="info-group">
                 <img src={phoneIcon}/>
-                <p>(212) 555-1234</p>
+                <p>{props.phone}</p>
             </div>
             <div className="info-group">
                 <img src={mailIcon} />
-                <p>mr.whiskaz@catnap.meow</p>
+                <p>{props.email}</p>
             </div>
         </div>
     </div>
