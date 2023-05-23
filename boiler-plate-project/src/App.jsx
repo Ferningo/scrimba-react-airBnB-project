84949fe4959aacc5772221e540e25b5026fddc30
Jose <jose.mar.123@hotmail.com>
2023-05-22 18:46:29 -0600

    Modify Joke component to use conditional rendering

--- boiler-plate-project/src/App.jsx
+++ boiler-plate-project/src/App.jsx
@@ -4,25 +4,24 @@ function App(){
   return (
     <>
       <Joke
+        punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+      />
+      <Joke
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
-      >
-      </Joke>
+      />
       <Joke
         setup="How did the hacker escape the police?"
         punchline="He just ransomware!"
-      >
-      </Joke>
+      />
       <Joke
         setup="Why don't pirates travel on mountain roads?"
         punchline="Scurvy."
-      >
-      </Joke>
+      />
       <Joke
         setup="Why do bees stay in the hive in the winter?"
         punchline="Swarm."
-      >
-      </Joke>
+      />
     </>
   )
 }
--- boiler-plate-project/src/components/Joke.jsx
+++ boiler-plate-project/src/components/Joke.jsx
@@ -1,14 +1,8 @@
 function Joke(props){
-    if (props.punchline === undefined){
-        return (
-        <div className="joke-container">
-            <div className="joke-punchline">{props.punchline}</div>
-        </div>
-        )
-    }
+
     return(
         <div className="joke-container">
-            <div className="joke-setup">{props.setup}</div>
+            {props.setup && <div className="joke-setup">{props.setup}</div>}
             <div className="joke-punchline">{props.punchline}</div>
         </div>
     )
