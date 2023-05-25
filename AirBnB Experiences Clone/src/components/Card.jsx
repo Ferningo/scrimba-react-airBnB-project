0f3e4179348be15e713a4438a5de3ff8e03e3281
Jose <jose.mar.123@hotmail.com>
2023-05-24 20:09:39 -0600

    Add logic to dinamically change badge value[C

--- AirBnB Experiences Clone/src/components/Card.jsx
+++ AirBnB Experiences Clone/src/components/Card.jsx
@@ -2,10 +2,17 @@
 import star from '../assets/star.png'
 
 export default function Card(props){
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
 
     return(
         <div className="card-container">
-            { props.openSpots <= 0 && <div className='badge'>SOLD OUT</div>}
+            { badgeText === "SOLD OUT" && <div className='badge'>{badgeText}</div>}
+            { badgeText === "ONLINE" && <div className='badge'>{badgeText}</div>}
             <img src={props.img} alt="" className='card-image' />
             <div className='stats-container'>
                 <img src={star} className='star-logo'/>
